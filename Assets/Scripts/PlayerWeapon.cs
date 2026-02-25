18d02aee4ff5a62980c0dd9e54c3636a0a4024d2
Soroush Alinia <soroushalinia.dev@gmail.com>
2026-02-25 13:53:10 +0330

    feat: shoot and aim laser

--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -6,8 +6,16 @@ public class PlayerWeapon : MonoBehaviour
 {
 
     [SerializeField] GameObject[] lasers;
+    [SerializeField] RectTransform crosshair;
+    [SerializeField] Transform targetPoint;
+    [SerializeField] float targetDistance;
 
     bool isFiring = false;
+
+    void Start()
+    {
+        Cursor.visible = false;
+    }
 
     void Update()
     {
@@ -16,6 +24,24 @@ public class PlayerWeapon : MonoBehaviour
             var emission = laser.GetComponent<ParticleSystem>().emission;
             emission.enabled = isFiring;
         }
+        if (Mouse.current != null)
+        {
+
+            var mousePosition = Mouse.current.position.ReadValue();
+            crosshair.position = mousePosition;
+
+            Vector3 targetPointPosition = new Vector3(mousePosition.x, mousePosition.y, targetDistance);
+
+            targetPoint.position = Camera.main.ScreenToWorldPoint(targetPointPosition);
+        }
+
+        foreach (var laser in lasers)
+        {
+            Vector3 fireDirection = targetPoint.position - this.transform.position;
+            Quaternion rotationToTarget = Quaternion.LookRotation(fireDirection);
+            laser.transform.rotation = rotationToTarget;
+        }
+
     }
 
     public void OnFire(InputValue value)
